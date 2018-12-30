a458d56469dd1d5401f0867cc00f41789ad2a485
Tyler Creller <tylercreller@gmail.com>
2018-12-30 18:01:25 -0500

    Force https

--- server/server.js
+++ server/server.js
@@ -2,8 +2,21 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000; // Heroku port or 3000
+const port = process.env.PORT || 3000; // Heroku port or 3000v
 
+function requireHTTPS(req, res, next) {
+	// The 'x-forwarded-proto' check is for Heroku
+	if (
+		!req.secure &&
+		req.get('x-forwarded-proto') !== 'https' &&
+		process.env.NODE_ENV !== 'development'
+	) {
+		return res.redirect('https://' + req.get('host') + req.url);
+	}
+	next();
+}
+
+app.use(requireHTTPS);
 app.use(express.static(publicPath));
 
 app.get('*', (request, response) => {
