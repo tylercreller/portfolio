b98d0875417e8e5454c1cb70f84af1dc993427ec
Tyler Creller <tylercreller@gmail.com>
2019-01-17 17:08:37 -0500

    Change projects to react component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,70 +21,72 @@ const styles = () => ({
 	}
 });
 
-function navigateToLink(link) {
-	window.open(link, '_blank');
-}
+class ProjectsGridList extends React.Component {
+	constructor(props) {
+		super(props);
+	}
 
-function ProjectsGridList(props) {
-	const { classes } = props;
+	render() {
+		const { classes } = this.props;
 
-	return (
-		<div className="projects" data-aos="fade-in">
-			<div className="large-text">Projects</div>
-			<div className={classes.root}>
-				<GridList cellHeight={180}>
-					{gridListData.map(tile => (
-						<GridListTile key={tile.img} className="projects__gridList__tile">
-							<img
-								src={tile.img}
-								alt={tile.title}
-								title={tile.title}
-								onClick={navigateToLink.bind(this, tile.site)}
-							/>
-							<GridListTileBar
-								className="projects__gridList__tile__bar"
-								title={
-									<div className="projects__gridList__tile__site-title">
-										{tile.title}
-									</div>
-								}
-								subtitle={
-									<span>
-										<a
-											className="projects__gridList__tile__site-link"
-											href={tile.site}
-											target="_blank"
-										>
-											{tile.site}
-										</a>
-									</span>
-								}
-								actionIcon={
-									<IconButton className={classes.button} disableRipple>
-										{tile.actionLink && (
+		return (
+			<div className="projects" data-aos="fade-in">
+				<div className="large-text">Projects</div>
+				<div className={classes.root}>
+					<GridList cellHeight={180}>
+						{gridListData.map(tile => (
+							<GridListTile key={tile.img} className="projects__gridList__tile">
+								<img
+									src={tile.img}
+									alt={tile.title}
+									title={tile.title}
+									onClick={navigateToLink.bind(this, tile.site)}
+								/>
+								<GridListTileBar
+									className="projects__gridList__tile__bar"
+									title={
+										<div className="projects__gridList__tile__site-title">
+											{tile.title}
+										</div>
+									}
+									subtitle={
+										<span>
 											<a
-												className="projects__gridList__tile__action-link"
-												href={tile.actionLink}
+												className="projects__gridList__tile__site-link"
+												href={tile.site}
 												target="_blank"
 											>
-												<i
-													className="fab fa-github fa-3x"
-													title="View on GitHub"
-												/>
+												{tile.site}
 											</a>
-										)}
-										<div className="projects__gridList__tile__tech">
-											{tile.tech}
-										</div>
-									</IconButton>
-								}
-							/>
-						</GridListTile>
-					))}
-				</GridList>
+										</span>
+									}
+									actionIcon={
+										<IconButton className={classes.button} disableRipple>
+											{tile.actionLink && (
+												<a
+													className="projects__gridList__tile__action-link"
+													href={tile.actionLink}
+													target="_blank"
+												>
+													<i
+														className="fab fa-github fa-3x"
+														title="View on GitHub"
+													/>
+												</a>
+											)}
+											<div className="projects__gridList__tile__tech">
+												{tile.tech}
+											</div>
+										</IconButton>
+									}
+								/>
+							</GridListTile>
+						))}
+					</GridList>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 ProjectsGridList.propTypes = {
