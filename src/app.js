925a421d26921d67dd69fcbdc315dd17151cb8ce
Tyler Creller <tylercreller@gmail.com>
2020-07-09 19:51:56 -0400

    Random Gifer

--- src/app.js
+++ src/app.js
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import AppRouter from './router/AppRouter';
+import RandomGif from './components/RandomGif';
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(
+	<BrowserRouter>
+		<Switch>
+			<Route path='/random-gif' exact={true} component={RandomGif} />
+			<Route path='/' component={AppRouter} />
+		</Switch>
+	</BrowserRouter>,
+	document.getElementById('app')
+);
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Particles from 'react-particles-js';
 
 import Header from '../components/Header';
@@ -11,51 +11,52 @@ import Projects from '../components/ProjectsV2';
 export default class Layout extends React.Component {
 	render() {
 		return (
-			<BrowserRouter>
-				<div style={{ height: '100%' }}>
-					<Particles
-						style={{ position: 'fixed', zIndex: -1 }}
-						params={{
-							particles: {
-								number: {
-									value: 100,
-									density: {
-										enable: true,
-										value_area: 1500
-									}
-								},
-								line_linked: {
+			<div style={{ height: '100%' }}>
+				<Particles
+					style={{ position: 'fixed', zIndex: -1 }}
+					params={{
+						particles: {
+							number: {
+								value: 100,
+								density: {
 									enable: true,
-									opacity: 0.05
-								},
-								move: {
-									direction: 'right',
-									speed: 0.05
-								},
-								size: {
-									value: 1
-								},
-								opacity: {
-									anim: {
-										enable: true,
-										speed: 1,
-										opacity_min: 0.05
-									}
+									value_area: 1500
 								}
 							},
-							retina_detect: true
-						}}
-					/>
-					<Header />
-					<Container>
+							line_linked: {
+								enable: true,
+								opacity: 0.05
+							},
+							move: {
+								direction: 'right',
+								speed: 0.05
+							},
+							size: {
+								value: 1
+							},
+							opacity: {
+								anim: {
+									enable: true,
+									speed: 1,
+									opacity_min: 0.05
+								}
+							}
+						},
+						retina_detect: true
+					}}
+				/>
+				<Header />
+				<Container>
+					<BrowserRouter>
 						<Switch>
-							<Route path="/" component={Projects} exact={true} />
-							<Route component={NotFoundPage} />
+							<Route path='/' component={Projects} exact={true} />
+							<Route path='/404' component={NotFoundPage} exact={true} />
+							<Redirect to='/404' />
 						</Switch>
-					</Container>
-					<Footer />
-				</div>
-			</BrowserRouter>
+					</BrowserRouter>
+				</Container>
+				<Footer />
+			</div>
 		);
 	}
 }
