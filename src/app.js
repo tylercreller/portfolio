bae20b42a1f38ca8841ec8441784fe701774de81
Tyler Creller <tylercreller@gmail.com>
2020-07-25 12:48:39 -0400

    Remove random gif page

--- src/app.js
+++ src/app.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import AppRouter from './router/AppRouter';
-import RandomGif from './components/RandomGif';
 
 ReactDOM.render(
 	<BrowserRouter>
 		<Switch>
-			<Route path='/random-gif' exact={true} component={RandomGif} />
 			<Route path='/' component={AppRouter} />
 		</Switch>
 	</BrowserRouter>,
